6512516a649743fbc9cd405afee15889b6f0b389
Zyrexnn <ikhsannovriansyah34@gmail.com>
2026-02-12 09:11:46 +0700

    Update Instagram URL and project links

--- src/sample.ts
+++ src/sample.ts
@@ -1,7 +1,7 @@
 const author = {
   name: `M. Khairul Haqqi`,
   nickname: `haqqi`,
-  ig: `https://www.instagram.com/kaigraph_`,
+  ig: `https://www.instagram.com/kaigraph_/`,
   wa: `https://wa.me/6289503953245`,
   email: "mailto:mkhairulhaqqi25@gmail.com",
 };
